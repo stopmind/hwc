c244c0186a40a7078cbbfe2a53f10ffa05097d25
stopmind <51329670+stopmind@users.noreply.github.com>
2024-10-12 12:52:17 +0300

    add images check

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,35 @@
 mod config;
 mod wallpapers_switcher;
 
-use device_query::{DeviceQuery, DeviceState};
+use device_query::{DeviceState};
+use log::error;
 use crate::wallpapers_switcher::WallpapersSwitcher;
 
 fn main() {
     env_logger::init();
     log_panics::init();
 
-    let device_state = DeviceState::new();
     let config = config::Config::load("./config.toml").unwrap();
+    let mut has_errors = false;
+    for preset in &config.presets {
+        let valid = match imghdr::from_file(preset.1.path.as_str()) {
+            Err(_) => false,
+            Ok(option) => option.is_some()
+        };
+
+        if !valid {
+            error!("Preset ({}) wallpaper file doesn't exist or isn't image.", preset.0);
+            has_errors = true;
+        }
+    }
+
+    if has_errors {
+        return;
+    }
+
     let mut switcher = WallpapersSwitcher::from_config(&config).unwrap();
 
+    let device_state = DeviceState::new();
     loop {
         switcher.check(&device_state);
     }
