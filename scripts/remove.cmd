fc74bd783bbf91fa032a9ce768610944158ddee9
stopmind <51329670+stopmind@users.noreply.github.com>
2024-10-12 21:46:39 +0300

    some fixes

--- scripts/remove.cmd
+++ scripts/remove.cmd
@@ -1,6 +1,6 @@
 @echo off
 
-rm "%AppData%\Microsoft\Windows\Start Menu\Programs\Startup\hwc.lnk"
+del "%AppData%\Microsoft\Windows\Start Menu\Programs\Startup\hwc.lnk"
 taskkill /f /t /im hwc.exe
 
 pause
